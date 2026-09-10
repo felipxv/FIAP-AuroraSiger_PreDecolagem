d37fa5ad42e772cd782eed681ee4fad8435b1f76
guilhermeSc <guilhermesantos3828@gmail.com>
2026-09-10 20:27:46 -0300

    add(Funções): Função de verificação de temperatura interna.

--- service.py
+++ service.py
@@ -1,1 +1,12 @@
 # nesse arquivo ficaram as funções de validação para Pre-decolagem
+
+def Vef_Temp_interna(Temp_interna, err):
+    print("Verificação de temperatura interna iniciada.")
+    if 18.0 <= Temp_interna <= 27.0:
+        print("Temperatura interna valida!")
+        err =+ 0
+        return Temp_interna, err
+    else:
+        print("Temperatura invalida!")
+        err =+ 1
+        return Temp_interna, err
