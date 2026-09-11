20fe44f0999f85108eb388fa502e5e3e87c80f71
guilhermeSc <guilhermesantos3828@gmail.com>
2026-09-10 21:43:11 -0300

    add(main): Instanciando variaveis: Análise energética.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-
-# Instanciando variaveis
+# Instanciando variaveis: Organização e descrição da telemetria
 temp_interna = float(input("Digite a temperatura interna(deve estar entre 18.0°C e 27.0°C ): "))
 temp_externa = float(input("Digite a temperatura externa(deve estar entre -60.0°C e 45°C): "))
 integridade = int(input("Digite 1 caso integridade Ok"))
-energia = int(input("Digite quantidade de energia disponivel(ela deve ser maior que 40%): "))
 presao_tanques = int(input("Digite a pressão dentro do tanque(deve estar entre 150 bar e 200 bar): "))
 modulos_crit = int(input("Digite 1 caso módulos críticos Ok"))
+
+# Instanciando variaveis: Análise energética
+# Capacidade total do sistema de energia, em kWh
+carga_Total = 150
+
+carga_atual = float(input("Digite o percentual de carga atual(deve ser maior que 40%): "))
+perdas = float(input("digite a porcentagem de perdas energeticas estimadas: "))
+
