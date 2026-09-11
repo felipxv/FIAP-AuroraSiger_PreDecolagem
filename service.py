4034ceb729e9ddec0c16dcf934b3ddbc8cc01c2c
felipxv <felipedebarrosfranco@gmail.com>
2026-09-10 21:21:29 -0300

    add(Funções): Função de verificação de integridade estrutural.

--- service.py
+++ service.py
@@ -16,3 +16,13 @@ def verificar_temperatura_externa(temp_externa):
     else:
         print("Temperatura externa inválida!")
         return False, f"Temperatura externa fora da faixa segura: {temp_externa}°C (esperado -60.0–45.0)"
+
+
+def verificar_integridade_estrutural(integridade):
+    print("Verificação de integridade estrutural iniciada.")
+    if integridade == 1:
+        print("Integridade estrutural válida!")
+        return True, None
+    else:
+        print("Integridade estrutural comprometida!")
+        return False, "Integridade estrutural comprometida (sensor reportou 0)"
